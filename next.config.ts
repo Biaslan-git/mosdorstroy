50593021f5494b450e585492eab8e85a078df98a
Biaslan <biaslan.pro@yandex.ru>
2026-05-16 17:40:54 +0300

    Add assetPrefix for images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/mosdorstroy",
+  assetPrefix: "/mosdorstroy",
   images: {
     unoptimized: true,
   },
